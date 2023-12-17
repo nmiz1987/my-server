05faeaddfb7a25592bb0cb4ad5573ed731a40a8b
Netanel Mizrahi <nmiz1987@gmail.com>
2023-12-17 23:00:55 +0200

    fix authenticateToken

--- authenticateToken.js
+++ authenticateToken.js
@@ -8,7 +8,7 @@ function authenticateToken(req, res, next) {
     if (token === undefined) {
       return res.status(401).json({ message: "Token is required" });
     }
-    jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, async (err, user) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
       if (err) {
         console.log(err);
         return res.status(401).json({ message: "Token expired" });
