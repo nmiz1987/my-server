a70d79ca9a216e40f1deaa546b977b948eb5e7e3
Netanel Mizrahi <nmiz1987@gmail.com>
2023-12-17 22:04:13 +0200

    fix status in login-with-token

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/login-with-token", auth, async (req, res) => {
     }
     res.status(200).json({ message: "success login", accessToken: user.accessToken, refreshToken: user.refreshToken });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(403).json({ message: err.message });
   }
 });
 
